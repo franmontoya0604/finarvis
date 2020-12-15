a50ce0af214bd26b28701a26fc5af78ebda9d6f4
franmontoya0604 <franmontoya00@hotmail.com>
2020-12-15 00:20:10 -0300

    progreso

---
+++ js.js
@@ -0,0 +1,33 @@
+(function ($) {
+    $(window).scroll(function (){
+        
+        
+        if(!$( ".fixed-top" ).hasClass( "show1" )){
+
+          var scroll = $(window).scrollTop();
+          
+        if (scroll>0) {
+            $("nav").addClass("change");
+            $("a#nav1").addClass(" change1")
+            $("nav div .c1").addClass(" change1");
+            $("nav .border-w").addClass(" change2");
+           
+            
+        }
+        else{
+            $("nav").removeClass("change");
+            $("a#nav1").removeClass("change1");
+            $("nav div .c1").removeClass(" change1");
+            $("nav .border-w").removeClass(" change2");
+        }
+      }
+        
+      
+    });
+
+
+    $( '[data-toggle="lightbox"]').click(function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+      });
+})
