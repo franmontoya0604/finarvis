c472500de68c30073318ec013d873240b2baf684
franmontoya0604 <franmontoya00@hotmail.com>
2020-12-21 01:34:19 -0300

    slider y nav

--- js.js
+++ js.js
@@ -2,23 +2,19 @@
     $(window).scroll(function (){
         
         
-        if(!$( ".fixed-top" ).hasClass( "show1" )){
+        if(!$( ".fixed-top" ).hasClass( "show" )){
 
           var scroll = $(window).scrollTop();
           
         if (scroll>0) {
             $("nav").addClass("change");
-            $("a#nav1").addClass(" change1")
-            $("nav div .c1").addClass(" change1");
-            $("nav .border-w").addClass(" change2");
+         
            
             
         }
         else{
             $("nav").removeClass("change");
-            $("a#nav1").removeClass("change1");
-            $("nav div .c1").removeClass(" change1");
-            $("nav .border-w").removeClass(" change2");
+       
         }
       }
         
@@ -30,5 +26,5 @@
         event.preventDefault();
         $(this).ekkoLightbox();
       });
-})
+})(jQuery);
 //preguntas alineamiento con container?    palabras slidershow    carousel por debajo de soluciones    nav cuando baja cambia de color?
