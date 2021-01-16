96e37095215f5a1ae4437a097661e0455da3a1f9
franmontoya0604 <franmontoya00@hotmail.com>
2021-01-16 00:11:07 -0300

    nav-mobile

--- js.js
+++ js.js
@@ -9,12 +9,12 @@
         if (scroll>0) {
             $("nav").addClass("change");
          
-           
+         
             
         }
         else{
             $("nav").removeClass("change");
-       
+           
         }
       }
         
@@ -22,9 +22,28 @@
     });
 
 
-    $( '[data-toggle="lightbox"]').click(function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-      });
+    $('#navbarToggleExternalContent').on('show.bs.collapse', function () {
+      $("#td").parent().addClass("show1");
+     
+      $("button svg.color-white").removeClass("fa-bars")
+      $("button svg.color-white").addClass("fa-times")
+      
+    
+
+      $(".c1").removeClass("change1")
+     
+        $("nav").removeClass("change");
+      
+    })
+
+    $('#navbarToggleExternalContent').on('hide.bs.collapse', function () {
+      $("#td").parent().removeClass("show1");
+      
+      $("button svg.color-white").addClass("fa-bars")
+      $("button svg.color-white").removeClass("fa-times")
+      
+    
+      
+    })
 })(jQuery);
 //preguntas alineamiento con container?    palabras slidershow    carousel por debajo de soluciones    nav cuando baja cambia de color?
